13758f81785beee6b5e83e4a7375b5e75c70b738
himitery <himitery@gmail.com>
2025-02-12 02:11:07 +0900

    feat: 비관적 락 적용
    - 동시성 이슈 해결

--- src/main/kotlin/dev/himitery/coupon/modules/coupon/adapter/out/persistence/CouponSetRepository.kt
+++ src/main/kotlin/dev/himitery/coupon/modules/coupon/adapter/out/persistence/CouponSetRepository.kt
@@ -1,6 +1,12 @@
 package dev.himitery.coupon.modules.coupon.adapter.out.persistence
 
 import dev.himitery.coupon.modules.coupon.domain.model.CouponSet
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import java.util.*
 
-interface CouponSetRepository : JpaRepository<CouponSet, String>
+interface CouponSetRepository : JpaRepository<CouponSet, String> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    override fun findById(id: String): Optional<CouponSet>
+}
